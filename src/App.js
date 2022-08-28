b07313cf5d445b91905a0e2500689c194a874327
Giradon <sch6523warz@outlook.kr>
2022-08-28 15:07:26 +0900

    feat: 라우팅 기능 추가
    홈, 장바구니, 상품상세정보 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Basket from "./pages/Basket";
+import ProductDetail from "./pages/ProductDetail";
 import './App.css';
-import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      코멘토 쇼핑</div>
+    <BrowserRouter basename="comento-shopping">
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/product/:productId" element = {<ProductDetail/>}/>
+        <Route path="/Basket" element = {<Basket />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
