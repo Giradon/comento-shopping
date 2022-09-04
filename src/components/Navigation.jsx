acf64b60dde8b2178a2611e8f927391c1e888b8d
Giradon <sch6523warz@outlook.kr>
2022-09-04 20:09:35 +0900

    design: 내비게이션 정렬

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,27 +1,27 @@
 import styled from "styled-components";
 
 const Navigation = () => {
-return <Navigationstyled>
-    {/* Navigation */}
-    <div>코멘토 쇼핑</div>
-</Navigationstyled>;    
+  return (
+    <Navigationstyled>
+      {/* Navigation */}
+      <div>코멘토 쇼핑</div>
+    </Navigationstyled>
+  );
 };
 
 const Navigationstyled = styled.div`
-width: 87px;
-height: 26px;
-left: 152px;
-top: 20px;
+  width: 87px;
+  height: 26px;
+  left: 152px;
+  top: 20px;
 
-font-weight: 700;
-font-size: 18px;
-line-height: 26px;
-
-display: flex;
-align-items: center;
-text-align: center;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 26px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  text-align: center;
 `;
-
-
 
 export default Navigation;
