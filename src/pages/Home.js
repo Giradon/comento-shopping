94561813c52eb1309152c0bc954086f3c7f713d1
Giradon <sch6523warz@outlook.kr>
2022-09-04 19:11:04 +0900

    feat: 테마버튼 눌렀을 때 상품 리스트 출력 로직 구현
    1. mock 상품 데이터를 가져와 홈화면 출력(map함수 사용)
    2. 테마 버튼 클릭 시 상품 리스트 변경(onClickThemeButton)
    3. 1초 뒤 상품 리스트 초기화(useEffect)
    4. 기타 포매팅 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,46 +2,78 @@ import Navigation from "../components/Navigation";
 import ThemeButton from "../components/ThemeButton";
 import ProductCard from "../components/ProductCard";
 import styled from "styled-components";
-const Home = ()=> {
-    return (
-    <div>        
-        <div>
-            <Navigation />
-            <ThemeSection>
-                <ThemeButton themeName={"#겨울방한템"} />
-                <ThemeButton themeName={"#따순머그컵"} />
-            </ThemeSection>
-            <Grayline/>
-        </div>
-        <ProductSection>
-            {/* ProductCard */}
-            <ProductCard name="비숑 블랙 머그잔" description="쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다." thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg"/>
-            
-            <ProductCard name="가열 보온 티코스터 온열 원터치 컵 받침대" description="언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따뜻한 차를 즐길 수 있습니다." thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg"/>
+import { mockTheme1Produdcts, mockTheme2Produdcts } from "../data/mockData";
+import { useState, useEffect } from "react";
 
-            <ProductCard name="벨루즈까사 솜사탕 파스텔 머그 4종 세트" description="솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그" thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg"/>       
-        </ProductSection>
+const Home = () => {
+  // 다시 렌더링되는 조건(state)
+  const [products, setProducts] = useState();
+
+  const onClickThemeButton = (themeId) => {
+    if (themeId === 1) {
+      setProducts(mockTheme1Produdcts);
+    } else if (themeId === 2) {
+      setProducts(mockTheme2Produdcts);
+    }
+  };
+  // 조건에 의해서 실행되는 함수
+  useEffect(() => {
+    setTimeout(() => {
+      setProducts(mockTheme1Produdcts);
+    }, 1000);
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <Navigation />
+        <ThemeSection>
+          <ThemeButton
+            themeName={"#겨울방한템"}
+            onClick={() => onClickThemeButton(1)}
+          />
+          <ThemeButton
+            themeName={"#여름더워요"}
+            onClick={() => onClickThemeButton(2)}
+          />
+        </ThemeSection>
+        <Grayline />
+      </div>
+      <ProductSection>
+        {/* mockData list를 화면에 노출하기 */}
+        {/* 삼향연산자 */}
+        {products ? (
+          products.map((product) => (
+            <ProductCard
+              key={product.id}
+              name={product.name}
+              description={product.description}
+              thumbnail={product.thumbnail}
+            />
+          ))
+        ) : (
+          <div>테마를 선택해주세요</div>
+        )}
+      </ProductSection>
     </div>
-    );
+  );
 };
 
 const ThemeSection = styled.div`
-display: flex;
-gap: 12px;
-padding: 40px 12px;
+  display: flex;
+  gap: 12px;
+  padding: 40px 12px;
 `;
 
 const Grayline = styled.div`
-    height: 8px;
-    width: 100%;
-    background: #eeeeee;
+  height: 8px;
+  width: 100%;
+  background: #eeeeee;
 `;
 
 const ProductSection = styled.div`
-    padding: 24px;
-    position: absolute;
+  padding: 24px;
+  position: absolute;
 `;
-
-
 
 export default Home;
