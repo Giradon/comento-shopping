ab04558b978642d16d51a4957b03a8376a8b4744
Giradon <sch6523warz@outlook.kr>
2022-09-04 18:23:16 +0900

    fix: ThemeButton.jsx 포매팅 수정

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 
-const ThemeButton = ({themeName}) => {
-    return <ThemeButtonStyled>{themeName}</ThemeButtonStyled>;
+const ThemeButton = ({ themeName, onClick }) => {
+  return <ThemeButtonStyled onClick={onClick}>{themeName}</ThemeButtonStyled>;
 };
 
 const ThemeButtonStyled = styled.div`
-background: rgba(0, 0, 0, 0.5);
-border-radius: 10px;
-width: fit-content;
-font-weight: 700;
-font-size: 20px;
-line-height: 26px;
-padding: 24px 16px;
-color:white;
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  width: fit-content;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+  padding: 24px 16px;
+  color: white;
 `;
 
 export default ThemeButton;
